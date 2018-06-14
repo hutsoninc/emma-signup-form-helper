a5afb61142cf25414bbefdd3ef868d87bf924afd
AustinLeeGordon <gordon.austin2@gmail.com>
2018-06-13 20:46:37 -0500

    Completed v1.0.0

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # emma-signup-form-helper
 
+https://emma-signup-form-helper.netlify.com/
+
+A helper for [react-emma-signup-form](https://github.com/hutsoninc/react-emma-signup-form). Used for fetching Emma signup form fields.
+
+## Requirements
+
+Must have [Allow-Control-Allow-Origin: *](https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi/related?hl=en_) Chrome extension installed and enabled.
+
 ## Authors
 
 * **Austin Gordon** - *Development* - [GitHub](https://github.com/AustinLeeGordon)
--- src/index.js
+++ src/index.js
@@ -1,4 +1,50 @@
-function createRequest(method, url){
+const fetchedContentEl = document.getElementById('fetched-content');
+const inputEl = document.getElementById('input');
+const outputElementsEl = document.getElementById('outputElements');
+const outputActionEl = document.getElementById('outputAction');
+const submitEl = document.getElementById('submit');
+
+var request;
+
+submitEl.addEventListener('mousedown', (el, ev) => {
+    
+    run();
+
+});
+
+inputEl.addEventListener('keypress', (el, ev) => {
+    
+    if(el.key.toLowerCase() == 'enter'){
+
+        run();
+
+    }
+
+});
+
+function run() {
+
+    request = createRequest('get', inputEl.value);
+
+    if (request){
+
+        request.onload = function() {
+    
+            fetchedContentEl.innerHTML = request.response;
+    
+            fetchData();
+    
+        };
+    
+        request.send();
+    
+    }else {
+        console.log('Error with request.');
+    }
+
+}
+
+function createRequest(method, url) {
 
     var xhr = new XMLHttpRequest();
 
@@ -11,14 +57,40 @@ function createRequest(method, url){
     
 }
 
-var request = createRequest("get", "https://signup.e2ma.net/signup/1875833/1743199/");
-
-if (request){
-
-    request.onload = function() {
-        console.log(request)
-    };
+function fetchData() {
     
-    request.send();
+    let output = [];
+    let outputAction;
+
+    const signupFormEl = document.getElementById('signup');
+
+    let fields = signupFormEl.getElementsByClassName('field-html');
+
+    fields = Array.prototype.slice.call(fields);
+
+    fields.map(el => {
+        output.push(el.children[0].outerHTML);
+    })
+
+    let formChildren = signupFormEl.children;
+
+    formChildren = Array.prototype.slice.call(formChildren);
+
+    formChildren.map(el => {
+        if(el.nodeName.toLowerCase() == 'input') {
+            output.push(el.outerHTML);
+        }
+    })
+
+    //output = output.join('').replace(/</g,'&lt;').replace(/>/g,'&gt;<br>');
+    output = output.join('').replace(/>/g,'>\n');
+
+    outputElementsEl.value = output;
+
+    outputAction = signupFormEl.action.split('/');
+
+    outputAction.pop();
+
+    outputActionEl.value = outputAction.join('/') + '/';
 
 }
